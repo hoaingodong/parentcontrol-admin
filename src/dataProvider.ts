3bc63decb43d489ae6ea2b25048932b1c9423e73
hoaingodong <hoaingodong1@gmail.com>
2023-07-20 11:07:48 +0700

    feat: just call upload data when having rawFile when updating

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,23 +1,23 @@
-import { DataProvider, fetchUtils } from "react-admin";
-import { stringify } from "query-string";
+import {DataProvider, fetchUtils} from "react-admin";
+import {stringify} from "query-string";
 
 const apiUrl = 'http://localhost:3000/api/v1';
 
 const httpClient = (url: string, options: any = {}) => {
     if (!options.headers) {
-        options.headers = new Headers({ Accept: 'application/json' });
+        options.headers = new Headers({Accept: 'application/json'});
     }
-    const { token }: { token: string } = JSON.parse(localStorage.getItem('auth'));
+    const {token}: { token: string } = JSON.parse(localStorage.getItem('auth'));
     options.headers.set('Authorization', `${token}`);
     return fetchUtils.fetchJson(url, options);
 };
 
 export const dataProvider: DataProvider = {
     getList: (resource, params) => {
-        const { page, perPage } = params.pagination;
-        let { field, order} = params.sort;
+        const {page, perPage} = params.pagination;
+        let {field, order} = params.sort;
 
-        if (order === "DESC"){
+        if (order === "DESC") {
             field = `-${field}`
         }
 
@@ -29,16 +29,16 @@ export const dataProvider: DataProvider = {
 
         const url = `${apiUrl}/${resource}?${stringify(query)}`;
 
-        return httpClient(url).then(({ headers, json }) => (
+        return httpClient(url).then(({headers, json}) => (
             {
-            data: json.data,
-            total: json.total,
-        }));
+                data: json.data,
+                total: json.total,
+            }));
     },
 
     getOne: (resource, params) =>
         httpClient(`${apiUrl}/${resource}/${params.id}`)
-            .then(({ json }) => {
+            .then(({json}) => {
                 const convertObject = {...json.data}
                 convertObject['id'] = convertObject['_id']
                 delete convertObject['_id']
@@ -48,10 +48,10 @@ export const dataProvider: DataProvider = {
             }),
 
     getMany: (resource, params) => {
-        const { ids } = params;
+        const {ids} = params;
 
         const promises = ids.map((id) =>
-            httpClient(`${apiUrl}/${resource}/${id}`).then(({ json }) => {
+            httpClient(`${apiUrl}/${resource}/${id}`).then(({json}) => {
                 const convertObject = {...json.data}
                 convertObject['id'] = convertObject['_id']
                 delete convertObject['_id']
@@ -62,13 +62,14 @@ export const dataProvider: DataProvider = {
         )
 
         return Promise.all(promises).then((responses) => (
-            {data: responses.map(response => response.data),
-        }));
+            {
+                data: responses.map(response => response.data),
+            }));
     },
 
     getManyReference: (resource, params) => {
-        const { page, perPage } = params.pagination;
-        const { field} = params.sort;
+        const {page, perPage} = params.pagination;
+        const {field} = params.sort;
         const query = {
             sort: field,
             limit: perPage,
@@ -76,7 +77,7 @@ export const dataProvider: DataProvider = {
         };
         const url = `${apiUrl}/${resource}?${stringify(query)}`;
 
-        return httpClient(url).then(({ headers, json }) => ({
+        return httpClient(url).then(({headers, json}) => ({
             data: json.data,
             total: json.total
         }));
@@ -91,7 +92,7 @@ export const dataProvider: DataProvider = {
                 method: 'POST',
                 body: fileData,
             })
-                .then(({ json }) => {
+                .then(({json}) => {
                     const updatedImageData = {
                         ...imageData,
                         [imageValue]: json.url,
@@ -101,8 +102,8 @@ export const dataProvider: DataProvider = {
                         method: 'POST',
                         body: JSON.stringify(updatedImageData),
                     })
-                        .then(({ json }) => ({
-                            data: { ...updatedImageData, id: json.id },
+                        .then(({json}) => ({
+                            data: {...updatedImageData, id: json.id},
                         }));
                 });
         };
@@ -115,7 +116,7 @@ export const dataProvider: DataProvider = {
                 method: 'POST',
                 body: fileData,
             })
-                .then(({ json }) => {
+                .then(({json}) => {
                     const updatedImageData = {
                         ...videoData,
                         [videoValue]: json.url,
@@ -125,22 +126,22 @@ export const dataProvider: DataProvider = {
                         method: 'POST',
                         body: JSON.stringify(updatedImageData),
                     })
-                        .then(({ json }) => ({
-                            data: { ...updatedImageData, id: json.id },
+                        .then(({json}) => ({
+                            data: {...updatedImageData, id: json.id},
                         }));
                 });
         };
 
         const uploadTwoImagesAndCreateResource = (imageData, valueImage1, valueImage2) => {
             const fileData = new FormData();
-            let updatedImageData = { ...imageData };
+            let updatedImageData = {...imageData};
 
             fileData.append('file', imageData[valueImage1].rawFile);
             return httpClient(`${apiUrl}/files/one`, {
                 method: 'POST',
                 body: fileData,
             })
-                .then(({ json }) => {
+                .then(({json}) => {
                     updatedImageData = {
                         ...updatedImageData,
                         [valueImage1]: json.url,
@@ -153,7 +154,7 @@ export const dataProvider: DataProvider = {
                         body: fileData,
                     });
                 })
-                .then(({ json }) => {
+                .then(({json}) => {
                     updatedImageData = {
                         ...updatedImageData,
                         [valueImage2]: json.url,
@@ -164,8 +165,8 @@ export const dataProvider: DataProvider = {
                         body: JSON.stringify(updatedImageData),
                     });
                 })
-                .then(({ json }) => ({
-                    data: { ...updatedImageData, id: json.id },
+                .then(({json}) => ({
+                    data: {...updatedImageData, id: json.id},
                 }));
         };
 
@@ -189,128 +190,156 @@ export const dataProvider: DataProvider = {
             method: 'POST',
             body: JSON.stringify(params.data),
         })
-            .then(({ json }) => ({
-                data: { ...params.data, id: json.id },
+            .then(({json}) => ({
+                data: {...params.data, id: json.id},
             }));
     },
 
     update: (resource, params) => {
         const uploadImageAndUpdateResource = (imageData, imageValue) => {
-            const fileData = new FormData();
-            fileData.append('file', imageData[imageValue].rawFile);
+            if (imageData[imageValue]?.rawFile) {
+                // If the file exists, proceed with image upload and resource update
+                const fileData = new FormData();
+                fileData.append('file', imageData[imageValue].rawFile);
 
+                return httpClient(`${apiUrl}/files/one`, {
+                    method: 'POST',
+                    body: fileData,
+                })
+                    .then(({json}) => {
+                        const updatedData = {
+                            ...imageData,
+                            [imageValue]: json.url,
+                        };
+
+                        return httpClient(`${apiUrl}/${resource}/${imageData.id}`, {
+                            method: 'PATCH',
+                            body: JSON.stringify(updatedData),
+                        })
+                            .then(() => ({
+                                data: updatedData,
+                            }));
+                    });
+            } else {
+                // If the file doesn't exist, return the original imageData without any modifications
+                return httpClient(`${apiUrl}/${resource}/${imageData.id}`, {
+                    method: 'PATCH',
+                    body: JSON.stringify(imageData),
+                })
+                    .then(() => ({
+                        data: imageData,
+                    }));
+            }
+        };
+
+        const uploadVideoAndUpdateResource = (videoData, videoValue) => {
+            if (videoData[videoValue]?.rawFile) {
+                // If the file exists, proceed with image upload and resource update
+                const fileData = new FormData();
+                fileData.append('file', videoData[videoValue].rawFile);
+
+                return httpClient(`${apiUrl}/files/file`, {
+                    method: 'POST',
+                    body: fileData,
+                })
+                    .then(({json}) => {
+                        const updatedData = {
+                            ...videoData,
+                            [videoValue]: json.url,
+                        };
+
+                        return httpClient(`${apiUrl}/${resource}/${videoData.id}`, {
+                            method: 'PATCH',
+                            body: JSON.stringify(updatedData),
+                        })
+                            .then(() => ({
+                                data: updatedData,
+                            }));
+                    });
+            } else {
+                // If the file doesn't exist, return the original imageData without any modifications
+                return httpClient(`${apiUrl}/${resource}/${videoData.id}`, {
+                    method: 'PATCH',
+                    body: JSON.stringify(videoData),
+                })
+                    .then(() => ({
+                        data: videoData,
+                    }));
+            }
+        };
+
+        const uploadTwoImagesAndUpdateResource = (imageData, valueImage1, valueImage2) => {
+            const fileData = new FormData();
+            let updatedImageData = {...imageData};
+
+            fileData.append('file', imageData[valueImage1].rawFile);
             return httpClient(`${apiUrl}/files/one`, {
                 method: 'POST',
                 body: fileData,
-            }).then(({ json }) => {
-                const updatedData = {
-                    ...imageData,
-                    [imageValue]: json.url,
-                };
+            })
+                .then(({json}) => {
+                    updatedImageData = {
+                        ...updatedImageData,
+                        [valueImage1]: json.url,
+                    };
 
-                return httpClient(`${apiUrl}/${resource}/${imageData.id}`, {
-                    method: 'PATCH',
-                    body: JSON.stringify({ [imageValue]: json.url }),
-                }).then(() => ({
-                    data: updatedData,
-                }));
-            });
+                    fileData.delete('file');
+                    fileData.append('file', imageData[valueImage2].rawFile);
+                    return httpClient(`${apiUrl}/files/one`, {
+                        method: 'POST',
+                        body: fileData,
+                    });
+                })
+                .then(({json}) => {
+                    updatedImageData = {
+                        ...updatedImageData,
+                        [valueImage2]: json.url,
+                    };
+
+                    return httpClient(`${apiUrl}/${resource}/${imageData.id}`, {
+                        method: 'PATCH',
+                        body: JSON.stringify(updatedImageData),
+                    })
+                        .then(() => ({
+                            data: updatedImageData,
+                        }));
+                })
         };
-
-        // const uploadTwoImagesAndUpdateResource = (imageData, valueImage1, valueImage2) => {
-        //     const fileData1 = new FormData();
-        //     const fileData2 = new FormData();
-        //
-        //     fileData1.append('file', imageData[valueImage1].rawFile);
-        //     fileData2.append('file', imageData[valueImage2].rawFile);
-        //
-        //     return Promise.all([
-        //         httpClient(`${apiUrl}/files/one`, { method: 'POST', body: fileData1 }),
-        //         httpClient(`${apiUrl}/files/one`, { method: 'POST', body: fileData2 }),
-        //     ]).then(([{ json: json1 }, { json: json2 }]) => {
-        //         const updatedData = {
-        //             ...imageData,
-        //             [valueImage1]: json1.url,
-        //             [valueImage2]: json2.url,
-        //         };
-        //
-        //         return httpClient(`${apiUrl}/${resource}/${imageData.id}`, {
-        //             method: 'PATCH',
-        //             body: JSON.stringify({
-        //                 [`${valueImage1}_id`]: json1.id,
-        //                 [`${valueImage2}_id`]: json2.id,
-        //             }),
-        //         }).then(() => ({
-        //             data: updatedData,
-        //         }));
-        //     });
-        // };
-        //
-        // // Helper function to upload video and update resource
-        // const uploadVideoAndUpdateResource = (videoData, videoValue) => {
-        //     const fileData = new FormData();
-        //     fileData.append('file', videoData[videoValue].rawFile);
-        //
-        //     return httpClient(`${apiUrl}/files/file`, {
-        //         method: 'POST',
-        //         body: fileData,
-        //     }).then(({ json }) => {
-        //         const updatedData = {
-        //             ...videoData,
-        //             [videoValue]: json.url,
-        //         };
-        //
-        //         return httpClient(`${apiUrl}/${resource}/${videoData.id}`, {
-        //             method: 'PATCH',
-        //             body: JSON.stringify({ [`${videoValue}_id`]: json.id }),
-        //         }).then(() => ({
-        //             data: updatedData,
-        //         }));
-        //     });
-        // };
-
-        // Check resource and data for file uploads and perform update
-        // if (resource === 'meals' && params.data.photos) {
-        //     return uploadImageAndUpdateResource(params.data, 'photos');
-        // }
 
         if (resource === 'music-categories' && params.data.background_url) {
             return uploadImageAndUpdateResource(params.data, 'background_url');
         }
 
-        // if (resource === 'audio' && params.data.url) {
-        //     return uploadVideoAndUpdateResource(params.data, 'url');
-        // }
-        //
-        // if (resource === 'screens' && params.data.app_icon && params.data.app_banner) {
-        //     return uploadTwoImagesAndUpdateResource(params.data, 'app_icon', 'app_banner');
-        // }
+        if (resource === 'audio' && params.data.url) {
+            return uploadVideoAndUpdateResource(params.data, 'url');
+        }
 
-        // If there are no file uploads, perform a regular update operation
+        if (resource === 'screens' && params.data.app_icon && params.data.app_banner) {
+            return uploadTwoImagesAndUpdateResource(params.data, 'app_icon', 'app_banner');
+        }
+
         return httpClient(`${apiUrl}/${resource}/${params.id}`, {
             method: 'PATCH',
             body: JSON.stringify(params.data),
-        }).then(({ json }) => {
-            const convertObject = { ...json.data };
-            convertObject['id'] = convertObject['_id'];
-            delete convertObject['_id'];
-            return {
-                data: convertObject,
-            };
-        });
+        })
+            .then(() => {
+                return {
+                    data: params.data,
+                };
+            });
     },
 
     delete: (resource, params) =>
         httpClient(`${apiUrl}/${resource}/${params.id}`, {
             method: 'DELETE',
-        }).then(({ json }) => ({ data: json })),
+        }).then(({json}) => ({data: json})),
 
     deleteMany: (resource, params) => {
         const deleteItemPromises = params.ids.map((id) => {
             const url = `${apiUrl}/${resource}/${id}`;
-            return httpClient(url, { method: "DELETE" }).then(({ json }) => json);
+            return httpClient(url, {method: "DELETE"}).then(({json}) => json);
         });
 
-        return Promise.all(deleteItemPromises).then(() => ({ data: params.ids }));
+        return Promise.all(deleteItemPromises).then(() => ({data: params.ids}));
     },
 };
